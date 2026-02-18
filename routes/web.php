c0b74bc2e4a88bced72d773ebb4b308b01a89d7e
ukakooo <>
2026-02-18 10:39:33 +0700

    Step 1 - 18 Exercise 1.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/welcome', function () {
+    return 'Welcome';
+});
+
+Route::get('/about', function () {
+    return '244107020215, Herconary Angga';
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'My name is ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Post to'.$postId." Comments to: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Article with ID '.$id;
+});
