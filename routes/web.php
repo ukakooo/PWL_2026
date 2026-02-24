52c27d8e4e004dd65a939bb4c91da7c401d165a5
Harutoo <herconaryangga15@gmail.com>
2026-02-24 16:26:05 +0700

    Controller steps

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class,'index']);
@@ -26,3 +27,6 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Post to'.$postId." Comments to: ".$commentId;
 });
 
+Route::resource('photos', PhotoController::class)->only(['index', 'show']);
+
+Route::resource('photos', PhotoController::class)->except(['create', 'store', 'update', 'destroy']);
